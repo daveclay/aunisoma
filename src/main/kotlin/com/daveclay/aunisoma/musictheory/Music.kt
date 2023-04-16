d276f59d1cd2920de8293d221b4fdc2dae2c65c3
Dave Clay <daveclay@gmail.com>
2023-04-15 22:27:31 -0700

    Remove unused code

--- src/main/kotlin/com/daveclay/aunisoma/musictheory/Music.kt
+++
@@ -1,9 +0,0 @@
-package com.daveclay.aunisoma.musictheory
-
-interface ChordBuilder {
-    fun fromRootNote(rootNote: Note): Array<Note>
-}
-
-class ChordInKey(val chord: Chord, val key: Key) {
-}
-
