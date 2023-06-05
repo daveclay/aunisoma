6aa5416c751377e78649d7287b6755369f40ca48
Dave Clay <david.clay@walmart.com>
2023-06-05 13:26:22 -0700

    Debug values to better spread gradient

--- python/main.py
+++ python/main.py
@@ -10,7 +10,7 @@ min_trigger_panel_animation_loop_duration_ticks = 200
 max_trigger_panel_animation_loop_duration_ticks = 500
 max_interaction_threshold_percent = .5
 max_interaction_duration_ticks = 500
-max_interaction_gradient_value_multiplier = .15 # spreads values across the gradient
+max_interaction_gradient_value_multiplier = .2 # spreads values across the gradient
 
 interaction_config = InteractionConfig(
     min_reverberation_distance,
@@ -29,8 +29,8 @@ gradient.add_rgb_point(.4,  255,   0,   0)
 gradient.add_rgb_point(1.0, 255, 255,   0)
 gradient.add_rgb_point(1.2,   0, 255,   0)
 gradient.add_rgb_point(1.5,   0, 128, 255)
-gradient.add_rgb_point(2.0,   0,   0, 255)
-gradient.add_rgb_point(2.7, 128,   0, 255)
+gradient.add_rgb_point(1.7,   0,   0, 255)
+gradient.add_rgb_point(2.1, 128,   0, 255)
 
 sensors = {}
 for i in range(number_of_panels):
--- python/panel.py
+++ python/panel.py
@@ -4,15 +4,16 @@ class Panel:
         self.interaction_active = False
         self.index = index
         self.idle_color = panel_context.gradient.get_color_for_value(0)
+        self.current_value = 0
         self.color = self.idle_color
-
-        self.set_color(self.idle_color)
+        self.reset()
 
     def to_dict(self):
         return {
             'index': self.index,
             'interactionActive': self.interaction_active,
             'idle_color': self.idle_color.to_dict(),
+            'currentValue': self.current_value,
             'color': self.color.to_dict()
         }
 
@@ -20,17 +21,20 @@ class Panel:
         self.interaction_active = active
 
     def reset(self):
+        self.current_value = 0
         self.set_color(self.idle_color)
 
     def update(self, panel_value_sources):
-        self.set_color(self.next_color(panel_value_sources))
+        value = self._get_value(panel_value_sources)
+        self.current_value = value
+        color = self.panel_context.gradient.get_color_for_value(value)
+        self.set_color(color)
 
     def set_color(self, color):
         self.color = color
 
-    def next_color(self, panel_value_sources):
+    def _get_value(self, panel_value_sources):
         value_sums = 0.0
         for panel_value_source in panel_value_sources:
             value_sums += panel_value_source.get_value_for_panel(self)
-
-        return self.panel_context.gradient.get_color_for_value(value_sums)
+        return value_sums
